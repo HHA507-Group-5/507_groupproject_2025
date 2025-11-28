e68604bddc7afec3e570c7567464e05ac9126a0c
angelh-16 <angelhuang0916@gmail.com>
2025-11-27 19:00:41 -0500

    started flags.py

---
+++ part4_flags.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+
+# Loading environment variables from .env file
+load_dotenv()
+
+sql_username = os.getenv('db_username')
+sql_password = os.getenv('db_password')
+sql_host = os.getenv('db_hostname')
+sql_database = os.getenv('db_database')
+
+if not all([sql_username, sql_password, sql_host, sql_database]):
+    raise ValueError("Missing one or more database environment variables.")
+
+# Creating the database connection string 
+url_string = f"mysql+pymysql://{sql_username}:{sql_password}@{sql_host}:3306/{sql_database}"
+TABLE = "research_experiment_refactor_test"
+
+# Creating engine
+engine = create_engine(url_string)
+
